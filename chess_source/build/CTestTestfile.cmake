bd73c3b3d76ab3868da54b90b760f2d651c7de1f
carlottapini <169172396+carlottapini@users.noreply.github.com>
2024-08-16 18:43:15 +0200

    Delete chess_source/build/CTestTestfile.cmake

--- chess_source/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carlottapini/progetto/chess_source
-# Build directory: /home/carlottapini/progetto/chess_source/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
