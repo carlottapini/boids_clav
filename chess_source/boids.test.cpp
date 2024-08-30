ed32d0bb947fe125e7929064c738043e0a7afbf3
Arcangelo Savasta <arcangelo.savasta@studio.unibo.it>
2024-08-30 18:30:30 +0200

    qualche test

--- chess_source/boids.test.cpp
+++ chess_source/boids.test.cpp
@@ -7,28 +7,28 @@
 #include "statistics.hpp"
 
 TEST_CASE("Testing pac-man effect") {
-    // Define the width and height of the boundary
-    float width{800.f};
-    float height{600.f};
+  // Define the width and height of the boundary
+  float width{800.f};
+  float height{600.f};
 
-    // Create a boid positioned outside the boundary with zero velocity
-    sf::Vector2f position(810.f, 610.f);
-    sf::Vector2f velocity(0.f, 0.f);
+  // Create a boid positioned outside the boundary with zero velocity
+  sf::Vector2f position(810.f, 610.f);
+  sf::Vector2f velocity(0.f, 0.f);
 
-    // Instantiate the boid with the given position and velocity
-    bd::Boid test_boid(position, velocity);
-    // Apply the Pac-Man effect to wrap the boid's position within the boundary
-    bd::pacman_effect(width, height, test_boid);
-    
-    // Check if the boid's position is correctly wrapped around to the boundary
-    CHECK(test_boid.getPosition().x == 0.f);
-    CHECK(test_boid.getPosition().y == 0.f);
-  }
-  TEST_CASE("Testing random velocity"){
-    float MAXSPEED{1.f};
+  // Instantiate the boid with the given position and velocity
+  bd::Boid test_boid(position, velocity);
+  // Apply the Pac-Man effect to wrap the boid's position within the boundary
+  bd::pacman_effect(width, height, test_boid);
+
+  // Check if the boid's position is correctly wrapped around to the boundary
+  CHECK(test_boid.getPosition().x == 0.f);
+  CHECK(test_boid.getPosition().y == 0.f);
+}
+
+TEST_CASE("Velocity's testing") {
+  float MAXSPEED{1.f};
 
   SUBCASE("Testing GenerateRdmSpeed") {
-    // Generate a random velocity with a maximum magnitude of MAXSPEED
     sf::Vector2f velocity = bd::GenerateRdmSpeed(MAXSPEED);
 
     if (velocity.x > 0)
@@ -42,22 +42,62 @@ TEST_CASE("Testing pac-man effect") {
       CHECK(velocity.y >= -MAXSPEED);
   }
 
-  SUBCASE("Testing limit velocity function"){
-    // Case 1: Speed ​​higher than maximum allowed
-    sf::Vector2f highVelocity(10.0f, 0.0f); // Speed ​​higher than MAXSPEED
+  SUBCASE("Testing limit velocity function") {
+    sf::Vector2f highVelocity(10.0f, 0.0f);  // Speed ​​higher than MAXSPEED
     bd::Boid boid1(sf::Vector2f(0.0f, 0.0f), highVelocity);
     limitVelocity(boid1, MAXSPEED);
-    
+
     // Check that the speed is limited to MAXSPEED
-    CHECK(std::hypot(boid1.getVelocity().x, boid1.getVelocity().y) == doctest::Approx(MAXSPEED).epsilon(0.01));
+    CHECK(std::hypot(boid1.getVelocity().x, boid1.getVelocity().y) ==
+          doctest::Approx(MAXSPEED).epsilon(0.01));
     // Case 2: Speed ​​already lower than the maximum permitted
-    sf::Vector2f lowVelocity(3.0f, 0.0f); // Speed ​​lower than max_speed
+    sf::Vector2f lowVelocity(0.5f, 0.0f);  // Speed ​​lower than max_speed
     bd::Boid boid2(sf::Vector2f(0.0f, 0.0f), lowVelocity);
-    
-    limitVelocity(boid2,MAXSPEED);
-    
+
+    limitVelocity(boid2, MAXSPEED);
+
     // Check that the speed has not changed
     CHECK(boid2.getVelocity() == lowVelocity);
+  }
+}
 
+TEST_CASE("Testing Boid compute_angle") {
+  bd::Boid boid{sf::Vector2f(0.f, 0.f), sf::Vector2f(0.f, 0.f)};
+
+  SUBCASE("Velocity in positive x direction") {
+    boid.setVelocity(sf::Vector2f(1.f, 1.f));  // Positive x and y
+    boid.compute_angle();
+    CHECK(boid.getRotation() ==
+          doctest::Approx(-12.5f +
+                          (180 / (static_cast<float>(M_PI))) * atanf(1.f / 1.f))
+              .epsilon(0.01));
+  }
+
+  SUBCASE("Velocity in negative x direction") {
+    boid.setVelocity(sf::Vector2f(-1.f, 1.f));  // Negative x and positive y
+    boid.compute_angle();
+    CHECK(boid.getRotation() ==
+          doctest::Approx(12.5f + (180 / (static_cast<float>(M_PI))) *
+                                      atanf(1.f / -1.f))
+              .epsilon(0.01));
+  }
+
+  SUBCASE("Velocity in positive y direction") {
+    boid.setVelocity(sf::Vector2f(0.f, 1.f));  // Zero x and positive y
+    boid.compute_angle();
+    CHECK(boid.getRotation() == doctest::Approx(90.f).epsilon(0.01));
+  }
+
+  SUBCASE("Velocity in negative y direction") {
+    boid.setVelocity(sf::Vector2f(0.f, -1.f));  // Zero x and negative y
+    boid.compute_angle();
+    CHECK(boid.getRotation() == doctest::Approx(-90.f).epsilon(0.01));
+  }
+
+  SUBCASE("Velocity at origin") {
+    boid.setVelocity(sf::Vector2f(0.f, 0.f));  // Zero velocity
+    boid.compute_angle();
+    CHECK(boid.getRotation() == doctest::Approx(0.f).epsilon(
+                                    0.01));  // Expecting rotation to be 0 since
   }
 }
